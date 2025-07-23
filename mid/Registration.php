8645939fa3277b087ada689609a0b409dafe7f8d
LT-Ripjaws <nightangle676x@gmail.com>
2025-07-23 12:17:13 +0600

    an update to Reg panel

--- mid/Registration.php
+++ mid/Registration.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html>
-    <title> This is a registration form</title>
+<head>
+    <title> Registration form</title>
+</head> 
     <body>
         <center>
             <h1 style = "color: blue;"> AIUB Registration Form</h1>
             <h2 style = "color: green;"> Please Fill The Form</h2>
+            <h4> Enter your information into the form</h4>
         </center>
-        <h4 align = "left"> Enter your information into the form</h4>
+        
 
         <style>
             body{background-color: lightblue;}
+            table{border: 1px solid black; margin: auto; background-color: white; padding: 20px}
+            
         </style>
         <form>
         <table>
@@ -21,20 +26,21 @@
             </tr>
             <tr>
                 <td> Select your language </td> 
-                <td> <input type = "radio" name = "des"> Bangla </td>
-                <td> <input type = "radio" name = "des"> English </td>
-                <td> <input type = "radio" name = "des"> Hindi </td>
+                <td> <input type = "radio" name = "des"> Bangla 
+                 <input type = "radio" name = "des"> English 
+                <input type = "radio" name = "des"> Hindi </td>
             </tr>
             <tr>
                 <td> Select your area </td>
-                <td> <input type = "checkbox" name = "des"> Dhaka </td>
-                <td> <input type = "checkbox" name = "des"> Barishal </td>
-                <td> <input type = "checkbox" name = "des"> Chittagong </td>
+                <td> <input type = "checkbox" name = "des"> Dhaka 
+                <input type = "checkbox" name = "des"> Barishal 
+                 <input type = "checkbox" name = "des"> Chittagong 
+            </td>
             </tr>
             <tr>
                 <td> Country: </td>
                 <td> <select name = "Country">
-                    <option value = ""> --Select- </option>
+                    <option value = ""> --Select-- </option>
                     <option value = "Bangladesh"> Bangladesh </option>
                     <option value = "India"> India </option>
                     <option value = "USA"> USA </option>
@@ -48,14 +54,16 @@
             </tr>
             <tr>
                 <td> Comments: </td>
-                <td> <textarea rows = "4" cols = "50" placeholder=" Write your comments here"> </textarea> </td>
+                <td> <textarea rows = "4" cols = "50" placeholder="Write your comments here"></textarea> </td>
             </tr>
         </table>
-</form>
+
         <br>
         <center>
-            <input type = "submit" value = "Submit" style = "background-color: rgb(100, 200, 120); color: white;">
+            <input type = "submit" value = "Submit">
+            <input type = "reset" value = "Reset">
         </center>
         <br>
+</form>
     </body>
 </html>
