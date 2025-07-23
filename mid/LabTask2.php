6eba1eba9601e748f51beb7c245df73c3716fb57
LT-Ripjaws <nightangle676x@gmail.com>
2025-07-23 12:32:16 +0600

    Labtask2

---
+++ mid/LabTask2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Customer Registration form</title>
+</head>
+    <style>
+
+    </style>
+
+<body>
+    <center>
+        <h1> Bank Management System</h1>
+        <h2> Your Trusted Financial Partner</h2>
+        <h3> Customer Registration Form</h3>
+    </center>
+    <form>
+        <table>
+            
+        </table>
+    </form>
